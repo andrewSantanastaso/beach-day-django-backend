fb17f67ec9066bcaae76d08056b8577ec6f20558
andrewSantanastaso <andrewsantanastaso@gmail.com>
2025-01-07 13:06:30 -0500

    setup api key

--- beachday/settings.py
+++ beachday/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c^p*f39p%mi7)_%4n(inlx93%-s)pd8k_o^7ag&y482p3g@((@'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Create your views here.
+secret=os.getenv('SECRET_KEY')
